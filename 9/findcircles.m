9d93df284e64cdac3e26b9b6c908f2bc5dc0359c
Ben Berman <ben.m.berman@gmail.com>
2014-04-28 21:21:58 -0500

    I believe I have hough results computing correctly

--- 9/findcircles.m
+++ 9/findcircles.m
@@ -1,35 +1,30 @@
 pkg load image
 
-orig = uint8(imread('adjusted.png')) * 256;
+orig = imread('img.bmp');
+
+filter_size = 50;
+avg_filt = ones(filter_size, filter_size) / (filter_size * filter_size);
+blurred = imfilter(orig, avg_filt);
+
+adjusted = !((orig > blurred) & (orig > 10));
+adjusted = adjusted .* 1.5;
 
 inner_size = 30;
 inner_filter = ones(inner_size, inner_size) / (inner_size * inner_size);
-inner = imfilter(orig, inner_filter);
+inner = imfilter(adjusted, inner_filter);
 
 disp('Inner');
 
 outer_size = 50;
 outer_filter = ones(outer_size, outer_size) / (outer_size * outer_size);
-outer = imfilter(orig, outer_filter); 
+outer = imfilter(adjusted, outer_filter); 
+
+disp('Outer');
 
 result = outer > (inner * 1.5);
-result = uint8(result);
-
-%filter_size = 50;
-%avg_filt = ones(filter_size, filter_size) / (filter_size * filter_size);
-%blurred = imfilter(orig, avg_filt);
-
-%adjusted = !((orig > blurred) & (orig > 10));
-%adjusted = adjusted .* 1.5;
+result = uint8(result) * 256;
 
 edges = edge(result);
 
-hresults = houghtf(result, 'circle', [30]);
-save('hresults.mat', 'hresults');
-
-imwrite(orig, 'orig.png');
-imwrite(inner, 'inner.png');
-imwrite(outer, 'outer.png');
-imwrite(result, 'result.png');
-%imwrite(edges, 'edge.png');
-%imwrite(adjusted, 'adjusted.png');
+hough_results = houghtf(result, 'circle', [30]);
+save('hough_results.mat', 'hough_results');
